98651d9ade0b29c37905dc1ff31d4e2b010d601b
OhSenungWan <81139055+OhSenungWan@users.noreply.github.com>
2022-02-19 22:26:02 +0900

    2022_02_19:애니메이션 추가

--- app/src/main/java/com/example/ai_caht/PlayActivity.kt
+++ app/src/main/java/com/example/ai_caht/PlayActivity.kt
@@ -1,11 +1,17 @@
 package com.example.ai_caht
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class PlayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status)
+
+
+        val ani = findViewById<ImageView>(R.id.ani)
+        Glide.with(this).load(R.raw.ani).into(ani)
     }
 }
