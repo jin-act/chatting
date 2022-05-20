6f95e060239403317028dd7ba7ea24703bb57faa
OhSenungWan <81139055+OhSenungWan@users.noreply.github.com>
2022-05-20 11:50:00 +0900

    2022_05_20: 애니메이션 바닥붙이기

--- app/src/main/java/com/example/ai_caht/TutorialActivity.kt
+++ app/src/main/java/com/example/ai_caht/TutorialActivity.kt
@@ -2,10 +2,15 @@ package com.example.ai_caht
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class TutorialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
+
+        val ani1 = findViewById<ImageView>(R.id.ani1)
+        Glide.with(this).load(R.raw.animated_stand).into(ani1)
     }
 }
