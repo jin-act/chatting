38fdea8752c09f1a21fb599ccf44e4134a9c37fb
DESKTOP-LTDRE58\82104 <jjin4073@naver.com>
2022-10-22 10:42:38 +0900

    2022-10-22(단순)
    로그인 링크 변동

--- app/src/main/java/com/example/ai_caht/test/RetrofitClient.java
+++ app/src/main/java/com/example/ai_caht/test/RetrofitClient.java
@@ -12,7 +12,7 @@ public class RetrofitClient {
     private static RetrofitClient instance = null;
     private static initMyApi initMyApi;
     //사용하고 있는 서버 BASE 주소
-    private static String baseUrl = "http://ec2-54-180-19-63.ap-northeast-2.compute.amazonaws.com:8080/";
+    private static String baseUrl = "https://aiparrot.shop/";
 
 
 
